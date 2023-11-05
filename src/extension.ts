2ec0fbf568d16bb6354c1dad5a3f37bec94ea4ad
Diego Lao Tebar <howarto@ymail.com>
2023-11-05 22:59:22 +0100

    Use modules import instead of all the package

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,34 @@
 import { exec } from 'child_process';
-import * as vscode from 'vscode';
+import { ExtensionContext, commands, window, workspace } from 'vscode';
 
 /**
  * Handler called on each call to the registered command.
  */
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand('phpstorm-formatter.formatPhpFile', () => {
-			const phpstormBinDir = vscode.workspace.getConfiguration('phpstorm-formatter').get('phpstormBinDir');
+		commands.registerCommand('phpstorm-formatter.formatPhpFile', () => {
+			const phpstormBinDir = workspace.getConfiguration('phpstorm-formatter').get('phpstormBinDir');
 
 			if (!phpstormBinDir) {
-				vscode.window.showErrorMessage(`PHPStorm Formatter could not format the document because phpstorm-formatter.phpstormBinDir configuration was not set.`);
+				window.showErrorMessage(`PHPStorm Formatter could not format the document because phpstorm-formatter.phpstormBinDir configuration was not set.`);
 				return;
 			}
 
-			const absoluteFilePath = vscode.window.activeTextEditor?.document.fileName;
+			const absoluteFilePath = window.activeTextEditor?.document.fileName;
 
 			if (!absoluteFilePath) {
-				vscode.window.showErrorMessage(`No active text editor.`);
+				window.showErrorMessage(`No active text editor.`);
 				return;
 			}
 
 			if (!absoluteFilePath.includes('.php')) {
-				vscode.window.showErrorMessage(`Only PHP files are supported.`);
+				window.showErrorMessage(`Only PHP files are supported.`);
 				return;
 			}
 
 			// I can't do more (like having a progress bar or something) because the script
 			// does not return an error or success code to the CLI.
-			vscode.window.showInformationMessage('PHPStorm formatter: Formatting file');
+			window.showInformationMessage('PHPStorm formatter: Formatting file');
 			exec(`"${phpstormBinDir}/phpstorm64.exe" format -allowDefaults "${absoluteFilePath}"`);
 		}),
 	);
